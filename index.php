8eeac0e14f88df65021f5b3f2c2f83b1e9b9d434
Breno Lins <36646169+BrenoLins100@users.noreply.github.com>
2020-11-17 12:42:18 -0300

    Atualização div e css

--- index.php
+++ index.php
@@ -1,47 +1,58 @@
+<?php
+
+    include_once ("recaptchalib.php");
+    error_reporting(0);
+    ini_set('display_errors', 0);
+
+?>
+
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="spectre.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-exp.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-icons.min.css">
     <link rel="stylesheet" href="style.css">
-    <title>Dupla autenticação</title>
+    <title>Recaptcha</title>
+
+    <script src="https://www.google.com/recaptcha/api.js?hl=pt-BR"></script>
+
 </head>
 <body>
-    <?php 
-        echo
-            "<form class='formulario' action='token.php' method='POST'>
 
-                <h2 id='titulo_form'>Insira seus dados para validação:</h2>
-                
-                <p id='aviso_p_validacao'>
-                    Aviso para validação!
-                </p>
-                <p>
-                    Após preencher seu campo de email, seremos capazes de enviar o código
-                    de segurança a você, e por meio desse código você conseguirá acessar
-                    a página de sucesso.
-                </p>
-                    
-                <label class='form-label' for='email'>Insira seu email:</label>
-                <input 
-                    class='form-input' 
-                    type='email' 
-                    name='email' placeholder='ex: seuEmail.@outlook.com'
-                    id='email' required>
-                     
-                <label class='form-label' for='email'>Insira seu nome completo:</label>
-                <input 
-                    class='form-input' 
-                    type='text' 
-                    name='nome_pessoa' placeholder='digite o seu nome.'
-                    id='email' required>
-                
-                <button id='btn_enviar' name='btn_enviar' class='btn btn-primary' type='submit'>
-                    Enviar código ao email
-                </button>
+    <div class="container">
+        <form method="POST" class="form-group">
+        <div class='toast toast-success'>Acesso permitido</div>
+        <div class='toast toast-error'>Acesso negado, resolva a reCaptcha</div>
+            <label for="email" class="form-label">Email:</label>
+            <input type="email" name="email" required class="form-input">
+            <label for="senha" class="form-label">Senha:</label>
+            <input type="password" name="senha"  class="form-input" required>
+            <br>
+            <div class="g-recaptcha" data-sitekey="6LfkAOQZAAAAAAGUkp-sipYI_qog-v7M6aLuFK-o"></div>
+            <button class="btn btn-primary" type="submit" >Entrar</button>
+            </form>
 
-            </form>";
+              <?php 
+            $secret = "6LfkAOQZAAAAAFaVSIsQidDSFdKBrl4kaVe-RcWa";
+            $response = null;
+            $reCaptcha = new reCaptcha($secret);
+
+            if(isset($_POST['g-recaptcha-response'])){
+                $response = $reCaptcha->verifyResponse($_SERVER['REMOTE_ADDR'],$_POST['g-recaptcha-response']);
+            }
+
+            if($response != null && $response->success){
+                echo "<script>document.querySelector('.toast-success').style.display = 'block'</script>";
+            }else{
+                echo "<script>document.querySelector('.toast-error').style.display = 'block'</script>";
+            }
         ?>
+      
+    </div>
+
 </body>
 </html>
