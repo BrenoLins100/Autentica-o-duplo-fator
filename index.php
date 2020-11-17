6df3dc111f995b5ad7680745c617e4b102bc2bcc
Breno Lins <36646169+BrenoLins100@users.noreply.github.com>
2020-11-17 15:41:33 -0300

    Css path fix

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/spectre.min.css">
-    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="spectre.min.css">
+    <link rel="stylesheet" href="style.css">
     <title>Dupla autenticação</title>
 </head>
 <body>
